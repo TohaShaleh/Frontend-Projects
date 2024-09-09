7594d93e0d29219336e0c059ca84a2aa40ba1f55
Abu Shaleh <abushalehtoha@gmail.com>
2024-09-09 14:39:25 +0600

    Update in Header Component

--- my_personal_website/Components/Header.js
+++ my_personal_website/Components/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <>
     
-    <div className='bg-slate-400 w-full h-[80px] flex justify-between p-[17px]'>
-      <Link className='text-3xl' href="/">Logo</Link>
+    <div className='bg-slate-400 w-full h-[80px] flex justify-between p-4'>
+      <Link className='text-4xl' href="/">Abu_Shaleh</Link>
        
       <div className='flex gap-10 p-3'>
 
         <Link className='hover:underline hover:font-bold ' href="/">About</Link>
-        <Link className='hover:underline hover:font-bold '  href="/Profile">Profile</Link>
+        <Link className='hover:underline hover:font-bold '  href="/ToDo">ToDo</Link>
         <Link className='hover:underline hover:font-bold '  href="/Resume">Resume</Link>
         <Link className='hover:underline hover:font-bold '  href="/Sign_in">Sign In</Link>
         <Link className='hover:underline hover:font-bold '  href="/Sign_up">Sign Up</Link>
