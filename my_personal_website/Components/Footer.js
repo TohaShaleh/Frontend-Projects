fa66a33bfce4aededdd4f04d78a7d2f83baa5db2
Abu Shaleh <abushalehtoha@gmail.com>
2024-09-11 02:13:44 +0600

    make responsive for all screens

--- my_personal_website/Components/Footer.js
+++ my_personal_website/Components/Footer.js
@@ -4,24 +4,43 @@ import Link from 'next/link'
 function Footer() {
   return (
     <>
-    <div className='flex flex-col justify-center items-center gap-5 bg-emerald-400 p-7'>
-        <h1 className='text-5xl'>Welcome To Footer Section</h1>
-        <h1 className='text-2xl'>Do You Have Any Query ? </h1>
+      <div className='flex flex-col justify-center items-center gap-5 bg-emerald-400 p-7'>
+        <h1 className='text-3xl md:text-5xl text-center'>Welcome To Footer Section</h1>
+
+        <h1 className='text-xl md:text-2xl text-center'>Do You Have Any Query?</h1>
+        
         <div className='flex flex-col justify-center items-center gap-5 w-full m-3'>
-            <h1 className='text-3xl'>Please Contact Us in the following ways </h1>
-            <div className='flex gap-4'>
 
-                <Link className='hover:bg-red-500 p-2' href="https://www.facebook.com">facebook</Link>
-                <Link className='hover:bg-red-500 p-2' href="https://www.youtube.com">youtube</Link>
-                <Link className='hover:bg-red-500 p-2' href="https://www.linkedin.com">linkedin</Link>
-                <Link className='hover:bg-red-500 p-2' href="https://github.com/TohaShaleh">GitHub</Link>
-                <Link className='hover:bg-red-500 p-2' href="https://leetcode.com/u/shaleh_31/">LeetCode</Link>
+          <h1 className='text-xl md:text-3xl text-center'>Please Contact Us in the following ways</h1>
+          
+          <div className='flex flex-wrap justify-center gap-4'>
 
-            </div>
+            <Link className='hover:bg-red-500 p-2 text-sm md:text-base rounded' href="https://www.facebook.com">
+              Facebook
+            </Link>
+
+            <Link className='hover:bg-red-500 p-2 text-sm md:text-base rounded' href="https://www.youtube.com">
+              YouTube
+            </Link>
+
+            <Link className='hover:bg-red-500 p-2 text-sm md:text-base rounded' href="https://www.linkedin.com">
+              LinkedIn
+            </Link>
+
+            <Link className='hover:bg-red-500 p-2 text-sm md:text-base rounded' href="https://github.com/TohaShaleh">
+              GitHub
+            </Link>
+
+            <Link className='hover:bg-red-500 p-2 text-sm md:text-base rounded' href="https://leetcode.com/u/shaleh_31/">
+              LeetCode
+            </Link>
+
+          </div>
+
+          {/* Added text-sm class for smaller screens and md:text-base for medium and larger screens */}
         </div>
-    </div>
+      </div>
     </>
-    
   )
 }
 
